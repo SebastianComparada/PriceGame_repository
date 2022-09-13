518d2237e9ecd271f9b1c6e3bb1e64b41dcb6276
ANIBAL COMPARADA <sebastian-capocomparada@hotmail.com>
2022-09-13 20:32:31 -0300

    Agregar archivos de proyecto.

---
+++ BackAPI/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public Response GetPrices(string name)
+        {
+            return PriceService.GetPrices();
+        }
+
+        [HttpPost]
+        public Response GetPrices(string name)
+        {
+            return PriceService.GetPrices();
+        }
+    }
+}
