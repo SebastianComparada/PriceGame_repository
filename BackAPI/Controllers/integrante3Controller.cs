c838eb5e1c43193b8da481b10ce91243eb1a7efd
Nico Intelangelo <nicointelangelo1@gmail.com>
2022-09-20 19:59:49 -0300

    integrante3push

---
+++ BackAPI/Controllers/integrante3Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class integrante3Controller : ControllerBase
+    {
+    }
+}
